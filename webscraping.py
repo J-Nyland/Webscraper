af8f17d4a8081de9c4bbd70b4e573e7a9e5b39f7
Jonathan Nyland <jonathan.nyland@cmsdistribution.com>
2020-05-05 16:29:30 +0100

    using search bar and scraping data w/ elements and tags

--- webscraping.py
+++ webscraping.py
@@ -1,4 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+Path = "C:\Program Files JN\chromedriver.exe"
+
+driver = webdriver.Chrome(Path)
+
+driver.get("https://techwithtim.net")
+print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+main = driver.find_elements_by_id("main")
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+    print(header.text)
+finally:
+    driver.quit()
+
+
+
+
+
+
+
+
+
+'''from selenium import webdriver
 
 Path = "C:\Program Files JN\chromedriver.exe"
 
@@ -7,4 +47,4 @@ driver = webdriver.Chrome(Path)
 driver.get("https://techwithtim.net")
 
 print(driver.title)
-driver.quit()
+driver.quit()'''
